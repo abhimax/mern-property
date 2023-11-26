bb625b6783943281222f5bf07b89a35230f94cc1
Abhiman Ranaweera <abhiman.max@gmail.com>
2023-11-26 11:27:29 +0530

    feat: connect to database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,16 @@
 import express from "express";
+import mongoose from "mongoose";
+import { configDotenv } from "dotenv";
+configDotenv();
+const connectDb = async () => {
+  try {
+    const respond = await mongoose.connect(process.env.MONGO_URL);
+    console.log("Connected to Mongo!");
+  } catch (error) {
+    console.log(error);
+  }
+};
+connectDb();
 const app = express();
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
