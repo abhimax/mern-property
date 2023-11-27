61aa94cfd31b51443b828a4bf182d7a80adfc3c8
Abhiman Ranaweera <abhiman.max@gmail.com>
2023-11-27 22:36:15 +0530

    feat: add test api route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,8 +1,7 @@
 import express from "express";
+import { test } from "../controllers/user.controllers.js";
 
 const router = express.Router();
 
-router.get("/test", (req, res) => {
-  res.json({ message: "my test api router" });
-});
+router.get("/test", test);
 export default router;
