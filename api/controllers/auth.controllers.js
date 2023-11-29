bbcfc74e8ed50dcc533266adb80570683df27a0e
Abhiman Ranaweera <abhiman.max@gmail.com>
2023-11-29 08:21:10 +0530

    feat: encrypt signup api rout password

--- api/controllers/auth.controllers.js
+++ api/controllers/auth.controllers.js
@@ -1,8 +1,18 @@
 import User from "../modals/user.model.js";
+import bcryptjs from "bcryptjs";
 
 export const signup = async (req, res) => {
   const { username, email, password } = req.body;
-  const newUser = new User({ username, email, password });
-  await newUser.save();
-  res.status(201).json("User created successfully!");
+  console.log("MONGO_HASH_KEY:", +process.env.MONGO_HASH_KEY);
+  const hashPassword = await bcryptjs.hashSync(
+    password,
+    +process.env.MONGO_HASH_KEY
+  );
+  const newUser = new User({ username, email, password: hashPassword });
+  try {
+    await newUser.save();
+    res.status(201).json("User created successfully!");
+  } catch (error) {
+    res.status(500).json(error.message);
+  }
 };
