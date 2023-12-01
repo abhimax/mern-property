b298bcc519bb056253ceeed1f3b15ed7fe5c04cc
Abhiman Ranaweera <abhiman.max@gmail.com>
2023-12-01 08:47:25 +0530

    feat: complete sign up functionality

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,14 +1,19 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 function SignUp() {
   const [formData, setFormData] = useState({});
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const navigate = useNavigate();
+
   const handleOnChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value });
   };
   const handleOnSubmit = async (e) => {
     e.preventDefault();
     console.log(formData);
+    setLoading(true);
     try {
       const res = await fetch("/api/auth/signup", {
         method: "POST",
@@ -18,9 +23,19 @@ function SignUp() {
         body: JSON.stringify(formData),
       });
       const data = await res.json();
+      setLoading(false);
+      if (data.success === false) {
+        setError(data.message);
+        return;
+      }
+      navigate("/sign-in");
+      setError(null);
+
       console.log(data);
     } catch (error) {
-      console.log(error);
+      setLoading(false);
+      setError(error);
+      return <div>ERROR</div>;
     }
   };
   return (
@@ -49,16 +64,20 @@ function SignUp() {
           onChange={handleOnChange}
         />
 
-        <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
-          Sign Up
+        <button
+          className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
+          disabled={loading}
+        >
+          {loading ? "Loading..." : "Sign UP"}
         </button>
       </form>
       <div className="flex gap-2 mt-5">
         <p>Have an account?</p>
         <Link to={"/sign-in"}>
-          <span className="text-blue-700">Sign in</span>
+          <span className="text-blue-700 disabled">Sign in</span>
         </Link>
       </div>
+      {error && <p className="text-red-500 mt-5">{error}</p>}
     </div>
   );
 }
